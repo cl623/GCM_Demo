e1adca2e3ed1bb7d6a7d2a4a9af9c3de2deee61a
Christian Laggui <claggui27@gmail.com>
2022-12-02 01:35:58 -0500

    Dynamic options per # of boxes. Fully functional. Need to add other inputs for the options.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
+/* Functions for carousel functionality */
 var slideIndex = 1;
 showSlides(slideIndex);
-
-var agentsDropdown = document.getElementById("agent");
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -31,7 +30,9 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   names[slideIndex-1].className += " active";
 }
+/* -------------------------------------*/
 
+/* Functions for disabling and enabling agent select dropdown*/
 function disableAgents(){
     agentsDropdown.disabled = true;
 }
@@ -39,16 +40,71 @@ function disableAgents(){
 function enableAgents(){
     agentsDropdown.disabled = false;
 }
+/* ----------------------------------------------------------*/
+
+/* Functions for dynamically adding options for boxes to be sent */
+
+var agentsDropdown = document.getElementById("agent");
+var options = ["Small", "Regular", "Jumbo", "Irregular"];
 
 function multiplyBoxes(){
-    var boxes = document.getElementsByName("newBoxes").values;
-    var 
-    var options = ["Small", "Regular", "Jumbo", "Irregular"]
-    for(i=0; i < boxes; i++){
-        var select = document.createElement("input");
-        select.id = "boxsize" + i;
+  //Get Number of Boxes Requested
+  var numBoxes = parseInt(document.getElementsByName("newBoxes")[0].value);
 
-        
-        
+  //Create Div for options per Box
+  var boxList = document.getElementsByClassName("boxList") ? document.getElementsByClassName("boxList").length : 0;
+
+  //IF number of options is greater than boxes requested (ie. they reduced their previous option)
+    //THEN remove last options
+  if(boxList > numBoxes){
+    removeBoxes((boxList-numBoxes));
+  }
+  else if(boxList == numBoxes){}
+    //ELSE add more options 
+  else{
+    createBoxes(boxList, numBoxes);
+  } 
+}
+
+function removeBoxes(n){
+  //Reference Parent Div
+  var boxDiv = document.getElementById("boxes");
+
+  //Remove last option set until number is reached
+  for(let i = 0; i < n; i++){
+    boxDiv.removeChild(boxDiv.lastChild);
+  }
+}
+
+function createBoxes(boxList, numBoxes){
+  //Reference Container Div
+  var boxDiv = document.getElementById("boxes");
+
+  for(let i = boxList; i < numBoxes; i++)
+    if(boxList < numBoxes){
+      //Create the containing Div for options
+      var nDiv = document.createElement("div");
+      nDiv.classList.add("boxList");
+      nDiv.id = "boxOptions" + (boxList + 1);
+
+      //Create Select Element
+      var select = document.createElement("select");
+      select.id = "boxsize" + (boxList + 1);
+
+      //Add options for Select
+      for(j=0; j < options.length; j++){
+        var option = document.createElement("option");
+        option.innerHTML = options[j];
+        option.value = options[j];
+        select.options.add(option);
+      }
+
+      //Append Select to Div
+      nDiv.appendChild(select);
+
+      //Append Div to Parent Container
+      boxDiv.appendChild(nDiv);
     }
 }
+
+/* ---------------------------------------------------------------------------------------*/
